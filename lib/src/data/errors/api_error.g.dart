5313a71fcf4dca52e09686102f63fcb8f9687f47
Wiso <wisosbenaty@gmail.com>
2024-12-20 15:07:05 +0400

    initial release

--- lib/src/data/errors/api_error.g.dart
+++ lib/src/data/errors/api_error.g.dart
@@ -10,8 +10,3 @@ ApiError _$ApiErrorFromJson(Map<String, dynamic> json) => ApiError(
       statusCode: (json['error_code'] as num).toInt(),
       apiErrorMessage: json['description'] as String,
     );
-
-Map<String, dynamic> _$ApiErrorToJson(ApiError instance) => <String, dynamic>{
-      'error_code': instance.statusCode,
-      'description': instance.apiErrorMessage,
-    };
